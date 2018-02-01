9a793efb47d825f5f98052e5231b686e4bc8e50f
liderong <715675812@qq.com>
2018-02-01 18:03:03 +0800

    Signed-off-by: liderong <715675812@qq.com>

---
+++ qhhhq-project/qhhhq-dao/merachant-dao/src/main/java/net/qhhhq/dao/merachant/MerachantActiveMapper.java
@@ -0,0 +1,8 @@
+package net.qhhhq.dao.merachant;
+
+import net.qhhhq.base.dao.BaseMapper;
+import net.qhhhq.model.merachant.MerachantActive;
+
+public interface MerachantActiveMapper extends BaseMapper<MerachantActive> {
+
+}
---
+++ qhhhq-project/qhhhq-dao/merachant-dao/src/main/java/net/qhhhq/dao/merachant/MerachantCurrentActiveMapper.java
@@ -0,0 +1,8 @@
+package net.qhhhq.dao.merachant;
+
+import net.qhhhq.base.dao.BaseMapper;
+import net.qhhhq.model.merachant.MerachantCurrentActive;
+
+public interface MerachantCurrentActiveMapper extends BaseMapper<MerachantCurrentActive> {
+
+}
