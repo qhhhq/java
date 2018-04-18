64a632d3c86c2695077c29ad0469764c48195513
qhhhq <715675812@qq.com>
2018-04-18 18:45:14 +0800

    Signed-off-by: qhhhq <715675812@qq.com>

--- weixin/mall/page/buycart/buycart.js
+++ weixin/mall/page/buycart/buycart.js
@@ -30,37 +30,11 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面隐藏
+   * 提交
    */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  submitOrder:function() {
+    wx.navigateTo({
+      url: '/page/buycart/pages/submit-order/submit-order',
+    })
   }
 })
