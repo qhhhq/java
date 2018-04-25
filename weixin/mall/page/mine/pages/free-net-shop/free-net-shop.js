fa5433ac538447b6439e10cf9c921ac6d645bf4a
qhhhq <715675812@qq.com>
2018-04-25 18:09:34 +0800

    Signed-off-by: qhhhq <715675812@qq.com>

--- weixin/mall/page/mine/pages/free-net-shop/free-net-shop.js
+++ weixin/mall/page/mine/pages/free-net-shop/free-net-shop.js
@@ -1,66 +1,64 @@
-// page/mine/pages/free-net-shop/free-net-shop.js
+//index.js
+
+// 获取显示区域长宽
+const device = wx.getSystemInfoSync()
+const W = device.windowWidth
+const H = device.windowHeight - 50
+
+let cropper = require('../../../../welCropper/welCropper.js');
+
+console.log(device)
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  onLoad: function () {
+    var that = this
+    // 初始化组件数据和绑定事件
+    cropper.init.apply(that, [W, H]);
   },
+  selectTap(e) {
+    let that = this
+    let mode = e.currentTarget.dataset.mode
+    console.log(e)
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+    wx.chooseImage({
+      count: 3,
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success(res) {
+        const tempFilePath = res.tempFilePaths[0]
+        console.log(tempFilePath)
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
+        // 将选取图片传入cropper，并显示cropper
+        that.showCropper({
+          src: tempFilePath,
+          mode: mode,
+          sizeType: ['original', 'compressed'],   //'original'(default) | 'compressed'
+          callback: (res) => {
+            if (mode == 'rectangle') {
+              console.log("crop callback:" + res)
+              wx.previewImage({
+                current: '',
+                urls: [res]
+              })
+            }
+            else {
+              wx.showModal({
+                title: '',
+                content: JSON.stringify(res),
+              })
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
+              console.log(res)
+            }
+             that.hideCropper() //隐藏，我在项目里是点击完成就上传，所以如果回调是上传，那么隐藏掉就行了，不用previewImage
+          }
+        })
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+      }
+    })
   }
 })
