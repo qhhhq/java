849088a944d7b7a7a1ce1c538fa438e4bf0126f8
qhhhq <715675812@qq.com>
2018-04-26 18:07:52 +0800

    Signed-off-by: qhhhq <715675812@qq.com>

--- weixin/mall/page/buycart/buycart.js
+++ weixin/mall/page/buycart/buycart.js
@@ -5,7 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    carts:[],
+    hasList: false,
+    totalPrice: 0,
+    selectAllStatus: true
   },
 
   /**
@@ -26,7 +29,24 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    this.setData({
+      hasList: true,        // 既然有数据了，那设为true吧
+      carts: [
+        { id: 1, title: '店铺1', selected: true,
+          items:[
+            { id: 11, name: '11111111111', title: '11111', img: '', price: '100.00', num: '1', selected: true },
+            { id: 12, name: '22222222222', title: '22222', img: '', price: '100.00', num: '1', selected: true },
+          ] 
+        },
+        {
+          id: 1, title: '店铺2', selected: true,
+          items: [
+            { id: 21, name: '11111111111', title: '11111', img: '', price: '100.00', num: '1', selected: true },
+            { id: 22, name: '22222222222', title: '22222', img: '', price: '100.00', num: '1', selected: true },
+          ]
+        }
+      ]
+    })
   },
 
   /**
@@ -42,5 +62,34 @@ Page({
     wx.navigateTo({
       url: '/page/common/pages/chat/chat',
     })
+  },
+
+  chooseItem:function(e) {
+    console.log(e)
+  },
+  selectList(e) {
+    const index = e.currentTarget.dataset.index;    // 获取data- 传进来的index
+    let carts = this.data.carts;                    // 获取购物车列表
+    const selected = carts[index].selected;         // 获取当前商品的选中状态
+    carts[index].selected = !selected;              // 改变状态
+    this.setData({
+      carts: carts
+    });
+    this.getTotalPrice();                           // 重新获取总价
+  },
+  getTotalPrice() {
+    let carts = this.data.carts;                  // 获取购物车列表
+    let total = 0;
+    for (let i = 0; i < carts.length; i++) {         // 循环列表得到每个数据
+      for (let j = 0; j < carts[i].items.length; j++) {
+        if (carts[i].items[j].selected) {         // 判断选中才会计算价格
+          total += carts[i].items[j].num * carts[i].items[j].price;   // 所有价格加起来
+        }
+      }
+    }
+    this.setData({                                // 最后赋值到data中渲染到页面
+      carts: carts,
+      totalPrice: total.toFixed(2)
+    });
   }
 })
--- weixin/mall/page/mine/pages/free-net-shop/free-net-shop.js
+++ weixin/mall/page/mine/pages/free-net-shop/free-net-shop.js
@@ -3,7 +3,8 @@
 // 获取显示区域长宽
 const device = wx.getSystemInfoSync()
 const W = device.windowWidth
-const H = device.windowHeight - 50
+//const H = device.windowHeight - 50
+const H = device.windowWidth
 
 let cropper = require('../../../../welCropper/welCropper.js');
 
@@ -13,12 +14,12 @@ Page({
   data: {
   },
   onLoad: function () {
-    var that = this
+    var self = this
     // 初始化组件数据和绑定事件
-    cropper.init.apply(that, [W, H]);
+    cropper.init.apply(self, [W, H]);
   },
   selectTap(e) {
-    let that = this
+    var self = this
     let mode = e.currentTarget.dataset.mode
     console.log(e)
 
@@ -27,38 +28,43 @@ Page({
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success(res) {
-        const tempFilePath = res.tempFilePaths[0]
-        console.log(tempFilePath)
-
-        // 将选取图片传入cropper，并显示cropper
-        that.showCropper({
-          src: tempFilePath,
-          mode: mode,
-          sizeType: ['original', 'compressed'],   //'original'(default) | 'compressed'
-          callback: (res) => {
-            if (mode == 'rectangle') {
-              console.log("crop callback:" + res)
-              wx.previewImage({
-                current: '',
-                urls: [res]
-              })
-            }
-            else {
-              wx.showModal({
-                title: '',
-                content: JSON.stringify(res),
-              })
-
-              console.log(res)
-            }
-             that.hideCropper() //隐藏，我在项目里是点击完成就上传，所以如果回调是上传，那么隐藏掉就行了，不用previewImage
-          }
-        })
-
-
-
-
+        const images = res.tempFilePaths
+        console.log(images)
+        self.imagesCropper(mode, images, 0)
       }
     })
+  },
+
+  imagesCropper: function (mode, images, index) {
+    console.log("index", index)
+    var self = this
+    if (images.length > index) {
+      // 将选取图片传入cropper，并显示cropper
+      self.showCropper({
+        src: images[index],
+        mode: mode,
+        sizeType: ['original', 'compressed'],   //'original'(default) | 'compressed'
+        callback: (res) => {
+          if (mode == 'rectangle') {
+            console.log("crop callback:" + res)
+            wx.previewImage({
+              current: '',
+              urls: [res]
+            })
+          }
+          else {
+            wx.showModal({
+              title: '',
+              content: JSON.stringify(res),
+            })
+
+            console.log(res)
+          }
+          
+          self.hideCropper() //隐藏，我在项目里是点击完成就上传，所以如果回调是上传，那么隐藏掉就行了，不用previewImage
+          self.imagesCropper(mode, images, ++index)
+        }
+      })
+    }
   }
 })
